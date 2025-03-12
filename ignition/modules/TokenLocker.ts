b4f75b5ef2a7f2d6698b11135426f789102eb50b
zouhair et-taousy <zouhirtaousy@gmail.com>
2025-03-12 07:01:52 +0100

    feat: update bulk sender/locker deploy

---
+++ ignition/modules/TokenLocker.ts
@@ -0,0 +1,14 @@
+import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
+
+import { ethers } from "hardhat";
+const tokenLockerModule = buildModule("TokenLocker", (m) => {
+
+  if(!process.env.OWNER_ADDRESS) {
+    throw new Error("OWNER_ADDRESS is required");
+  }
+  const bulkSender = m.contract("TokenLocker", [ethers.parseEther("0.001"), process.env.OWNER_ADDRESS]);
+
+  return { bulkSender };
+});
+
+export default tokenLockerModule;
