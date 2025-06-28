aefadea163b02a09f56acb2bfcfc2eaaf4227b6f
dawar2151 <zouhirtaousy@gmail.com>
2025-06-28 22:21:36 +0200

    feat: add token creation fee in env

--- ignition/modules/TokenCreator.ts
+++ ignition/modules/TokenCreator.ts
@@ -2,11 +2,15 @@ import { buildModule } from '@nomicfoundation/hardhat-ignition/modules';
 
 const TokenCreatorModule = buildModule('TokenCreator', (m) => {
   var ownerAddress = process.env.OWNER_ADDRESS;
+  var tokenCreatorFee = process.env.TOKEN_CREATOR_FEE;
+  if (!tokenCreatorFee) {
+    throw new Error('Token creator fee is not set in environment variables');
+  }
   if (!ownerAddress) {
     throw new Error('Owner address is not set in environment variables');
   }
   const tokenCreator = m.contract('TokenCreator', [
-    ethers.parseEther('0.0001'),
+    ethers.parseEther(tokenCreatorFee),
     ownerAddress,
   ]);
 
